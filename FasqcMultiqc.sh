ce3a6422bcd655259ae226a87461dc68a721e962
Marilyne-AzaGnandji <maryona@outlook.fr>
2019-05-09 14:11:51 +0200

    Adding to FastqcMultiqc.sh: how i install fastqc and multiqc

--- FasqcMultiqc.sh
+++ FasqcMultiqc.sh
@@ -6,7 +6,15 @@
 #
 # indicate here or in another script the command lines used to
 # install fastqc and multiqc
-#
+#To install fastqc
+apt-get install fastqc
+#To install multiqc, i use this alternative:
+git clone https://github.com/ewels/Multiqc.git
+#then
+cd MultiQC
+#then to install its setup.py
+python setup.py install
+
 OUTPUT_FOLDER="FastqcResults"
 mkdir ${OUTPUT_FOLDER}  # what if the folder already exists?
 
