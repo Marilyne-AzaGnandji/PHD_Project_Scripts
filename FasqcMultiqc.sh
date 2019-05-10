0abef0a4e7f488e26e54010a53c66a694126f666
Marilyne-AzaGnandji <38045144+Marilyne-AzaGnandji@users.noreply.github.com>
2019-05-10 13:28:27 +0200

    Update FasqcMultiqc.sh

--- FasqcMultiqc.sh
+++ FasqcMultiqc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -
-
-# quality control with fastqc
+#Test if fastqc and multiqc are available on your computer
+fastq #or fastqc -h or fastqc --help for more details
+multiqc #or multiqc -h or multiqc --help for more details
 
 # how to install?
 #
@@ -21,6 +22,7 @@ OUTPUT_FOLDER="FastqcResults"
 cat ${OUTPUT_FOLDER}
 #In case this folder is not available;we can create easily one:
 mkdir ${OUTPUT_FOLDER}
+# quality control with fastqc
 for f in *fastq.gz ; do
     fastqc --outdir ./${OUTPUT_FOLDER}/ "${f}"
 done
