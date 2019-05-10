49d34eac9a6d098a7b624128fc64e81aa43eef6b
Marilyne-AzaGnandji <maryona@outlook.fr>
2019-05-10 15:37:16 +0200

    I add a test to check existence of adapters in my paired-en-reads sequences

--- atropos.sh
+++ atropos.sh
@@ -1,11 +1,18 @@
-#!/bin/bash
+#!/bin/bash -
 
 cd /home/u082-f048/Bureau/marilyne/PhD_Thesis/SAMA_12_first_10k_reads
-
-  #Perform quality trimming
-  for f in *_R1.fastq.gz;
-  do
-   atropos trim -q 20,20 -o output1.fastq -p output2.fastq -pe1 "${f}" -pe2 "${f/R1/R2}"
-  done
+#Detect potential adapters in my paired-en-reads
+for f in *_R1.fastq.gz;
+do
+atropos detect -pe1 "${f}" -pe2 "${f/R1/R2}"
+done>AdapterDetection.txt
+#Perform quality trimming
+#SampleNames=
+ # OUTPUT_FILE1=*_R1.fastq.gz
+#  for f in *_R1.fastq.gz;
+ # do
+  # atropos trim -q 20,20 -o ${OUTPUT_FILE1}.fastq -p ${OUTPUT_FILE2}.fastq -pe1 "${f}" -pe2 "${f/R1/R2}"
+ # done
 
 exit 0
+
