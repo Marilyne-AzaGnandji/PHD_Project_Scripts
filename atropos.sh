5ca4a2a9f71ff9cd3bf4478d7842545c2ba9fb89
Marilyne-AzaGnandji <38045144+Marilyne-AzaGnandji@users.noreply.github.com>
2019-05-12 21:09:56 +0200

    Update atropos.sh

--- atropos.sh
+++ atropos.sh
@@ -5,14 +5,14 @@ cd /home/u082-f048/Bureau/marilyne/PhD_Thesis/SAMA_12_first_10k_reads
 #Test to detect potential adapters in my paired-en-reads
 for f in *_R1.fastq.gz;
 do
-atropos detect -pe1 "${f}" -pe2 "${f/R1/R2}"
-done>AdapterDetection.txt
+  atropos detect -pe1 "${f}" -pe2 "${f/R1/R2}"
+done
 #Perform quality trimming
 for f in *_R1.fastq.gz;
 do
-OUTPUT_FILE1="${f:0:-9}"
-OUTPUT_FILE2="${OUTPUT_FILE1/R1/R2}"
-atropos trim -q 20,20 -o ${OUTPUT_FILE1}.fastq -p ${OUTPUT_FILE2}.fastq -pe1 "${f}" -pe2 "${f/R1/R2}"
+  OUTPUT_FILE1="${f:0:-9}"
+  OUTPUT_FILE2="${OUTPUT_FILE1/R1/R2}"
+  atropos trim -q 20,20 -o ${OUTPUT_FILE1}.fastq -p ${OUTPUT_FILE2}.fastq -pe1 "${f}" -pe2 "${f/R1/R2}"
 done
 
 exit 0
