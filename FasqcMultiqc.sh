e1156448e33a4dabf1cc40275f343fc59f0777e3
Marilyne-AzaGnandji <38045144+Marilyne-AzaGnandji@users.noreply.github.com>
2019-05-12 21:07:40 +0200

    Update FasqcMultiqc.sh

--- FasqcMultiqc.sh
+++ FasqcMultiqc.sh
@@ -8,13 +8,13 @@ multiqc #multiqc --version or multiqc -h or multiqc --help for more details
 # indicate here or in another script the command lines used to
 # install fastqc and multiqc
 #To install fastqc
-apt-get install fastqc
-#To install multiqc, i use this alternative:
-git clone https://github.com/ewels/Multiqc.git
+  ##apt-get install fastqc
+#To install multiqc, i used this alternative:
+  ##git clone https://github.com/ewels/Multiqc.git
 #then
 cd MultiQC
 #then to install its setup.py
-python setup.py install
+ ##python setup.py install
 
 OUTPUT_FOLDER="FastqcResults"
 # what if the folder already exists?
