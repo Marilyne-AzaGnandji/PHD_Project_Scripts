9ad797e30cdd4c79cdeaf4b1d6587da8a716f030
Marilyne-AzaGnandji <38045144+Marilyne-AzaGnandji@users.noreply.github.com>
2019-05-20 13:41:40 +0200

    Update FastqcMultiqc_Installation.sh

--- FastqcMultiqc_Installation.sh
+++ FastqcMultiqc_Installation.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# indicate here or in another script the command lines used to
+# All informations about fastqc downloading and installation are available here: 
+   ##https://www.bioinformatics.babraham.ac.uk/projects/fastqc/
 # install fastqc and multiqc
-#To install fastqc
+#To install fastqc, i used this alternative
 apt-get install fastqc
-#To install multiqc, i use this alternative:
+#All informations about multiqc downloading and installation are available here:
+ ##https://github.com/ewels/MultiQC
+#To install multiqc, i used this alternative:
 git clone https://github.com/ewels/Multiqc.git
 #then
 cd MultiQC
