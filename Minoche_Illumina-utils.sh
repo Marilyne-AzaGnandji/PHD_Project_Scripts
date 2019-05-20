36f8b2bfa598d430566c645839a3b77c9baf971f
Marilyne-AzaGnandji <38045144+Marilyne-AzaGnandji@users.noreply.github.com>
2019-05-20 11:43:02 +0200

    Update Minoche_Illumina-utils.sh

--- Minoche_Illumina-utils.sh
+++ Minoche_Illumina-utils.sh
@@ -2,12 +2,11 @@
 
 cd /home/u082-f048/Bureau/marilyne/PhD_Thesis/SAMA_12_first_10k_reads
 #create a file named samples.txt
+(printf "sample\tr1\tr2\n"
 for R1 in *R1.fastq.gz ; do
   echo -e "${R1/_*/}\t${R1}\t${R1/R1/R2}"
 done > samples.txt
-#headers add
-sed -i $'1 i\\\nsample\tr1\tr2' samples.txt
-cat samples.txt
+#cat samples.txt
 #create config files for illumina-utils 
 iu-gen-configs samples.txt -o 01_QC/
 #run minoche quality filtering for each sample
