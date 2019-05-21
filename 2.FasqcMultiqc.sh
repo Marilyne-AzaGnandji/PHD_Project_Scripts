50a1f8a279a138e3f47d2d7152957c8d92fb3c99
Marilyne-AzaGnandji <38045144+Marilyne-AzaGnandji@users.noreply.github.com>
2019-05-21 12:56:26 +0200

    Update 2.FasqcMultiqc.sh

--- 2.FasqcMultiqc.sh
+++ 2.FasqcMultiqc.sh
@@ -1,18 +1,13 @@
-
-#!/bin/bash
-# how to install?
- ##You will see all informations about fastqc and multiqc installation in FastqcMultiqc_installation.sh
+#!/bin/bash -
+#You will see all informations about fastqc and multiqc installation in FastqcMultiqc_installation.sh
 #Test if fastqc and multiqc are available on your computer
-fastq #fastqc --version or fastqc -h or fastqc --help for more details
-multiqc #multiqc --version or multiqc -h or multiqc --help for more details
-<<<<<<< HEAD
-=======
-
->>>>>>> 36f8b2bfa598d430566c645839a3b77c9baf971f
+which fastq && echo "OK" || "not installed"
+which multiqc && echo "OK" || "not installed"
 OUTPUT_FOLDER="FastqcResults"
-# what if the folder already exists?
-##we can check this folder and its content like this:
-cat ${OUTPUT_FOLDER}
+#we can check this folder and its content like this:
+sudo find / -type d -name "${OUTPUT_FOLDER}"
+#In case this directory is found, do:
+rm -r ${OUTPUT_FOLDER} #to delete
 #In case this folder is not available;we can create easily one:
 mkdir ${OUTPUT_FOLDER}
 # quality control with fastqc
