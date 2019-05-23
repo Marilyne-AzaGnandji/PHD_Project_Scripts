469b6bbfb963f60c9e55ccf224e96d56c78b8f35
Marilyne-AzaGnandji <38045144+Marilyne-AzaGnandji@users.noreply.github.com>
2019-05-23 13:16:03 +0200

    Update 0.Softwares_Installation.sh

--- 0.Softwares_Installation.sh
+++ 0.Softwares_Installation.sh
@@ -12,21 +12,22 @@ git clone https://github.com/ewels/Multiqc.git
 sudo apt-get install python
 sudo apt install python-pip
 #then
-cd MultiQC
+cd Multiqc/
 #then to install its setup.py
 sudo python setup.py install
 #Atropos installation
 #Python3.3+ at least and cython0.25.2+ are required to install atropos
 sudo apt-get update
-sudo apt-get install python3.6 #python3.6 installation
-sudo apt-get cython #cython installation 
-#In case there is an old version of python, you can make available python3.6:
+sudo apt-get install python3 #python3.7 installation(my current update version)
+sudo apt-get install cython #cython installation 
+#In case there is an old version of python, you can make available python3.7:
 update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
-update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2 #when you put "2",you give priority to python version 3.6 that is currently needed
+update-alternatives --install /usr/bin/python python /usr/bin/python3.7 2 #when you put "2",you give priority to python version 3.7 that is currently needed
 #To install atropos
-sudo pip install atropos #sudo apt install python3-pip if not installed
+sudo apt install python3-pip #sudo apt install python3-pip if not installed
+sudo pip install atropos 
 #To install illumina-utils
-pip install illumina-utils
+sudo pip install illumina-utils
 
 
 
