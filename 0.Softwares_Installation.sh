c4373ca362c65875dfe848ea35d93d94a954e5e1
Frédéric Mahé <frederic.mahe@cirad.fr>
2019-05-31 13:04:03 +0200

    code review
    - mix of apt and apt-get, pick one,
    - run an `update` before any `apt install` 
    - use empty lines to make your code cleaner,
    - use `# comment` rather than `#comment`
    - don't you need to be sudo to run `update-alternatives`?
    - does `illumina-utils` needs `pip2` or `pip3`?
    - use `python2` or `python3` rather than `python` (avoid ambiguity)
    
    Please apply the same principles to the other scripts in this repository.

--- 0.Softwares_Installation.sh
+++ 0.Softwares_Installation.sh
@@ -1,34 +1,30 @@
 #!/bin/bash -
-# All informations about fastqc downloading and installation are available here: 
-   ##https://www.bioinformatics.babraham.ac.uk/projects/fastqc/
-# install fastqc and multiqc
-#To install fastqc, i used this alternative
-sudo apt-get install fastqc
-#All informations about multiqc downloading and installation are available here:
- ##https://github.com/ewels/MultiQC
-#To install multiqc, i used this alternative:
-git clone https://github.com/ewels/Multiqc.git
-#python(version 2 at least),pip(to get for instance setuptools packages) are required
-sudo apt-get install python
-sudo apt install python-pip
-#then
-cd Multiqc/
-#then to install its setup.py
-sudo python setup.py install
-#Atropos installation
-#Python3.3+ at least and cython0.25.2+ are required to install atropos
+
+## install fastqc
+# (see https://www.bioinformatics.babraham.ac.uk/projects/fastqc/)
 sudo apt-get update
-sudo apt-get install python3 #python3.7 installation(my current update version)
-sudo apt-get install cython #cython installation 
-#In case there is an old version of python, you can make available python3.7:
+sudo apt-get install fastqc
+
+## install multiqc
+# (see https://github.com/ewels/MultiQC)
+# requires python 2+, pip (for setuptools packages)
+sudo apt-get update
+sudo apt-get install python2 python-pip
+git clone https://github.com/ewels/Multiqc.git
+cd ./Multiqc/
+sudo python2 setup.py install
+
+## install atropos
+# (see https://github.com/jdidion/atropos)
+# requires python3.3+ and cython0.25.2+
+sudo apt-get update
+sudo apt-get install python3 cython python3-pip # currently python 3.7 and cython 0.28
+sudo pip3 install atropos
+# give priority to python 3 over python 2:
 update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
-update-alternatives --install /usr/bin/python python /usr/bin/python3.7 2 #when you put "2",you give priority to python version 3.7 that is currently needed
-#To install atropos
-sudo apt install python3-pip #sudo apt install python3-pip if not installed
-sudo pip install atropos 
-#To install illumina-utils
+update-alternatives --install /usr/bin/python python /usr/bin/python3.7 2 # 2 has priority over 1
+
+## install illumina-utils
 sudo pip install illumina-utils
-
-
 
 exit 0
