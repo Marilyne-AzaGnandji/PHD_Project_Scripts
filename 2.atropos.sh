6364b0dbe2ad402c08ef8ef788f165d1ec37538f
Frédéric Mahé <mahe@rhrk.uni-kl.de>
2019-06-11 09:58:02 +0200

    code review

--- 2.atropos.sh
+++ 2.atropos.sh
@@ -1,26 +1,26 @@
 #!/bin/bash -
 
-cd /home/u082-f048/Bureau/marilyne/PhD_Thesis/SAMA_12_first_10k_reads
+cd ${HOME}/Bureau/marilyne/PhD_Thesis/SAMA_12_first_10k_reads/
 
 # Test if atropos is available on your computer
-which atropos && echo "atropos is installed!" ||
-if echo "!!! atropos is not installed";
-  then exit 1;
-fi
+which atropos && \
+    echo "atropos is installed!" || \
+        { echo "Error: atropos is not installed" ; exit 1 ; }
 
-# Detect potential adapters in my paired-end-reads
-for f in *_R1.fastq.gz;
-do
-  atropos detect -pe1 "${f}" -pe2 "${f/R1/R2}"
-done
-
-# Perform quality trimming
-for f in *_R1.fastq.gz;
-do
+# Perform quality trimming (use full length option names)
+for f in *_R1.fastq.gz ; do
   OUTPUT_FILE1="${f:0:-9}"
   OUTPUT_FILE2="${OUTPUT_FILE1/R1/R2}"
-  atropos trim -q 20,20 -o ${OUTPUT_FILE1}.fastq -p ${OUTPUT_FILE2}.fastq -pe1 "${f}" -pe2 "${f/R1/R2}"
+  atropos \
+      trim \
+      -q 20,20 \
+      -o ${OUTPUT_FILE1}.fastq \
+      -p ${OUTPUT_FILE2}.fastq \
+      -pe1 "${f}" \
+      -pe2 "${f/R1/R2}"
 done
+
+## objective: file_R1.fastq.gz -> file_trimmed_R1.fastq.gz
 
 exit 0
 
