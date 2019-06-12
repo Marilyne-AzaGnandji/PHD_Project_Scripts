148992d0b9c5895e97e77a1727a345fc0aa6aa1b
Marilyne-AzaGnandji <maryona@outlook.fr>
2019-06-12 09:33:20 +0200

    2.atropos.sh updating

--- 2.atropos.sh
+++ 2.atropos.sh
@@ -9,16 +9,16 @@ which atropos && \
 
 # Perform quality trimming (use full length option names)
 for f in *_R1.fastq.gz ; do
-    #OUTPUT_FILE1="${f:0:-9}"
-    OUTPUT_FILE1="${f/_*/}"
-    OUTPUT_FILE2="$OUTPUT_FILE1"
+    OUTPUT_FILE1=${f/R1/trimmed_R1}
+    OUTPUT_FILE2=${f/R1/trimmed_R2}
+   #echo "$OUTPUT_FILE1"
     
 ## objective: file_R1.fastq.gz -> file_trimmed_R1.fastq.gz   
   atropos \
       trim \
       --quality-cutoff 20,20 \
-      --output ${OUTPUT_FILE1}_trimmed_R1.fastq.gz \
-      --paired-output ${OUTPUT_FILE2}_trimmed_R2.fastq.gz \
+      --output ${OUTPUT_FILE1} \
+      --paired-output ${OUTPUT_FILE2} \
       --input1 "${f}" \
       --input2 "${f/R1/R2}"
 done
