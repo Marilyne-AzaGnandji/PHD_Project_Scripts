e81906dcf54c2f2dcfd8cb4c86a918fea98ebe67
Marilyne-AzaGnandji <38045144+Marilyne-AzaGnandji@users.noreply.github.com>
2019-06-18 09:35:54 +0200

    Update 3.Minoche_Illumina-utils.sh

--- 3.Minoche_Illumina-utils.sh
+++ 3.Minoche_Illumina-utils.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -
 
-cd /home/u082-f048/Bureau/marilyne/PhD_Thesis/SAMA_12_first_10k_reads
+cd ${HOME}/Bureau/marilyne/PhD_Thesis/SAMA_12_first_10k_reads
 
 # Test if iu-filter-quality-minoche is available on your computer
 which iu-filter-quality-minoche && \
