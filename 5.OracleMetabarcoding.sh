576f3713c809b4b78fe5e3461755dec2ca6ad179
marilyne <38045144+Marilyne-AzaGnandji@users.noreply.github.com>
2020-07-17 14:15:44 +0000

    Adding "OTU table creation" sections

--- 5.OracleMetabarcoding.sh
+++ 5.OracleMetabarcoding.sh
@@ -2,9 +2,9 @@
 
 ### We want to use Fred's metabarcoding pipeline to obtain OTU table from FASTQ files (with swarm)
 
-cd $HOME/Documents/Marilyne/PhD_Thesis/SAMA_12_first_10k_reads/Metabarcoding
+#cd $HOME/Documents/Marilyne/PhD_Thesis/SAMA_12_first_10k_reads/Metabarcoding
 ## Check quality encoding (33 or 64?) : Our data are ARN 16S metadata
-
+cd $HOME/Documents/marilyne/Metabarcoding/
 # Test if vsearch is available on your computer
 VSEARCH=$(which vsearch) && \
     echo "vsearch is installed!" || \
@@ -29,7 +29,7 @@ for f in *R1_001.fastq.gz ; do
     FORWARD=$f
     REVERSE=${f/R1/R2}
     OUTPUT=${f/_*/_assembled.fastq}
-    echo "${VSEARCH}" \
+     "${VSEARCH}" \
 	 --threads ${THREADS} \
 	 --fastq_mergepairs ${FORWARD} \
 	 --reverse ${REVERSE} \
@@ -39,9 +39,10 @@ for f in *R1_001.fastq.gz ; do
 	 --quiet 2> ${OUTPUT/.fastq/.log}
 done 
 
-cd $HOME/Documents/Marilyne/PhD_Thesis/SAMA_12_first_10k_reads/Metabarcoding
+#cd $HOME/Documents/Marilyne/PhD_Thesis/SAMA_12_first_10k_reads/Metabarcoding
+cd $HOME/Documents/marilyne/Metabarcoding/
 ##  primer clipping, sample dereplication and quality extraction
-set -x
+#set -x
 
 # Define binaries, temporary files and output files
 MIN_LENGTH=32
@@ -124,10 +125,9 @@ done
 # Clean
 rm -f "${INPUT_REVCOMP}" "${TMP_FASTQ}" "${TMP_FASTA}" "${TMP_FASTQ2}" "${OUTPUT}"
 
-cd $HOME/Documents/Marilyne/PhD_Thesis/SAMA_12_first_10k_reads/Metabarcoding
+#cd $HOME/Documents/Marilyne/PhD_Thesis/SAMA_12_first_10k_reads/Metabarcoding
 ## Global dereplication, clustering and chimera detection
-set -x
-
+cd $HOME/Documents/marilyne/Metabarcoding/
 VSEARCH=$(which vsearch)
 SWARM=$(which swarm)
 TMP_FASTA=$(mktemp --tmpdir=".")
@@ -137,9 +137,8 @@ FINAL_FASTA="combined_samples.fas"
 #cat *.fas > "${TMP_FASTA}" 
 
 # Dereplicate (vsearch)
-
 cat *.fas | \
-    vsearch \
+    "${VSEARCH}" \
         --derep_fulllength - \
         --sizein \
 	--sizeout \
@@ -147,41 +146,143 @@ cat *.fas | \
 	--output "${FINAL_FASTA}" > /dev/null
 
 # "${VSEARCH}" --derep_fulllength "${TMP_FASTA}" \
-#              --sizein \
-#              --sizeout \
-#              --fasta_width 0 \
-#              --output "${FINAL_FASTA}" > /dev/null
+    #              --sizein \
+    #              --sizeout \
+    #              --fasta_width 0 \
+    #              --output "${FINAL_FASTA}" > /dev/null
 
 #rm -f "${TMP_FASTA}"
 
-cd $HOME/Documents/Marilyne/PhD_Thesis/SAMA_12_first_10k_reads/Metabarcoding
-set -x
+#cd $HOME/Documents/Marilyne/PhD_Thesis/SAMA_12_first_10k_reads/Metabarcoding
+cd $HOME/Documents/marilyne/Metabarcoding/
 # Clustering
 SWARM=$(which swarm) #
 VSEARCH=$(which vsearch) #
-THREADS=3
+THREADS=16
 TMP_REPRESENTATIVES=$(mktemp --tmpdir=".")
 FINAL_FASTA="combined_samples.fas"
 "${SWARM}" \
-      -d 1 -t ${THREADS} -z \
-      -i ${FINAL_FASTA/.fas/_1f.struct} \
-      -s ${FINAL_FASTA/.fas/_1f.stats} \
-      -w ${TMP_REPRESENTATIVES} \
-      -o ${FINAL_FASTA/.fas/_1f.swarms} < ${FINAL_FASTA}
+    -d 1 --fastidious -y 16 -b 3 -t ${THREADS} -z \
+    -i ${FINAL_FASTA/.fas/_1f.struct} \
+    -s ${FINAL_FASTA/.fas/_1f.stats} \
+    -w ${TMP_REPRESENTATIVES} \
+    -o ${FINAL_FASTA/.fas/_1f.swarms} < ${FINAL_FASTA}
 
 
 # Sort representatives
-
 "${VSEARCH}" --fasta_width 0 \
              --sortbysize ${TMP_REPRESENTATIVES} \
              --output ${FINAL_FASTA/.fas/_1f_representatives.fas}
 rm ${TMP_REPRESENTATIVES}
-
 # Chimera checking
 REPRESENTATIVES=${FINAL_FASTA/.fas/_1f_representatives.fas}
 UCHIME=${REPRESENTATIVES/.fas/.uchime}
 "${VSEARCH}" --uchime_denovo "${REPRESENTATIVES}" \
              --uchimeout "${UCHIME}"
+
+# Define variables, temporary files and output files
+cd $HOME/Documents/marilyne/Metabarcoding/
+RELEASE=132
+URL="https://www.arb-silva.de/fileadmin/silva_databases/release_${RELEASE}/Exports"
+INPUT="SILVA_${RELEASE}_SSURef_Nr99_tax_silva.fasta.gz"
+PRIMER_F_NAME="341F_785R"
+OUTPUT="${INPUT/.fasta.gz/}_${PRIMER_F_NAME}.fas"
+LOG="${OUTPUT/.fas/.log}"
+PRIMER_F="CCTACGGGNGGCWGCAG"
+PRIMER_R="GGATTAGATACCCBDGTAGTC"
+MIN_LENGTH=32
+MIN_F=$(( ${#PRIMER_F} * 2 / 3 ))
+MIN_R=$(( ${#PRIMER_R} * 2 / 3 ))
+CUTADAPT="cutadapt --discard-untrimmed --minimum-length ${MIN_LENGTH}"
+
+# Download
+[[ -f "${INPUT}" ]] || wget \
+     --no-clobber \
+     --continue \
+     "${URL}/${INPUT}" "${URL}/${INPUT}.md5"
+
+# Check
+md5sum -c "${INPUT}.md5"
+
+# Trim forward & reverse primers
+zcat "${INPUT}" | sed '/^>/ ! s/U/T/g' | \
+     ${CUTADAPT} -g "${PRIMER_F}" -O "${MIN_F}" - 2> "${LOG}" | \
+     ${CUTADAPT} -a "${PRIMER_R}" -O "${MIN_F}" - 2>> "${LOG}" | \
+     sed '/^>/ s/;/|/g ; /^>/ s/ /_/g ; /^>/ s/_/ /1' > "${OUTPUT}"
+
+# Stats
+grep -c "^>" "${OUTPUT}"
+set -x
+cd $HOME/Documents/marilyne/Metabarcoding/
+FINAL_FASTA="combined_samples_1f_representatives.fas"
+DATABASE="SILVA_132_SSURef_Nr99_tax_silva_341F_785R.fas"
+FILTER=2
+THREADS=2
+VSEARCH=$(which vsearch)
+set -x "${VSEARCH}" \
+     --fastx_filter "${FINAL_FASTA}" \
+     --minsize "${FILTER}" \
+     --quiet \
+     --fastaout - | \
+ set -x  "${VSEARCH}" \
+     --usearch_global - \
+     --threads ${THREADS} \
+     --dbmask none \
+     --qmask none \
+     --rowlen 0 \
+     --notrunclabels \
+     --userfields query+id1+target \
+     --maxaccepts 0 \
+     --maxrejects 32 \
+     --top_hits_only \
+     --output_no_hits \
+     --db ${DATABASE} \
+     --id 0.5 \
+     --iddef 1 \
+     --userout - > hits.representatives
+set -x
+ # in case of multi-best hit, find the last-common ancestor
+cd $HOME/Documents/marilyne/Metabarcoding/
+python3 ./stampa_merge.py $(pwd)
+
+# sort by decreasing abundance
+cd $HOME/Documents/marilyne/Metabarcoding/
+RESULTS="tailhitsrepresentatives.txt"
+sort -k2,2nr -k1,1d results.representatives > "${RESULTS}"
+
+# clean
+rm {hits,results}.representatives
+
+# Build the OTU table
+set -x
+cd $HOME/Documents/marilyne/Metabarcoding/
+FASTA="combined_samples.fas"
+SCRIPT="OTU_contingency_table.py"
+STATS="combined_samples_1f.stats"
+SWARMS="combined_samples_1f.swarms"
+REPRESENTATIVES="combined_samples_1f_representatives.fas"
+UCHIME="combined_samples_1f_representatives.uchime"
+ASSIGNMENTS="results.representatives"
+QUALITY="combined_samples.qual"
+OTU_TABLE="${FASTA/.fas/.OTU.table}"
+
+ python \
+    "${SCRIPT}" \
+    "${REPRESENTATIVES}" \
+    "${STATS}" \
+    "${SWARMS}" \
+    "${UCHIME}" \
+    "${QUALITY}" \
+    "${ASSIGNMENTS}" \
+  OR-*.fas [T]-*.fas > "${OTU_TABLE}"
+
+ # Filter the OTU table
+cd $HOME/Documents/marilyne/Metabarcoding/
+TABLE="combined_samples.OTU.table"
+FILTERED="${TABLE/.table/.filtered.table}"
+
+head -n 1 "${TABLE}" > "${FILTERED}" 
+cat "${TABLE}"| awk '$7 == "N" && $9 <= 0.0002 && ($2 >= 3 || $8 >= 2)' >> "${FILTERED}"
 
 exit 0
 
